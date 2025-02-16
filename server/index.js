3d832628882ce279cfdf4999fed0a3e0a95d8eb8
Mahendra Chandel <mahendrachandel040@gmail.com>
2025-02-16 21:53:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ console.log(process.env.PORT)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173',
+app.use(cors({ origin: 'https://dhabha-front.onrender.com',
     allowedHeaders: ["Content-Type", "Authorization", "token"],
 
 
